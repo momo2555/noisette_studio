0e9f6f3972899229b3b5ad3c9da64db578c39496
momo2555 <shadowrapplique@gmail.com>
2020-06-19 16:29:00 +0200

    add freeplan xml project mind map

---
+++ Noisette Studio.mm
@@ -0,0 +1,159 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Noisette Studio" FOLDED="false" ID="ID_1885195084" CREATED="1592563697751" MODIFIED="1592566772180" COLOR="#ffffff" BACKGROUND_COLOR="#ff6666" STYLE="oval" NUMBERED="true">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="0.622">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Gestion du StoryBoard" POSITION="right" ID="ID_1719361075" CREATED="1592563750595" MODIFIED="1592566769157" HGAP_QUANTITY="126.49999664723883 pt" VSHIFT_QUANTITY="62.999998122453746 pt">
+<edge COLOR="#ff0000"/>
+<node TEXT="Création d&apos;une TimeLine" ID="ID_1884470536" CREATED="1592563863487" MODIFIED="1592565992502" HGAP_QUANTITY="48.499998971819906 pt" VSHIFT_QUANTITY="-61.49999816715723 pt"/>
+<node TEXT="Logline-  Résumé" ID="ID_1798843430" CREATED="1592563911129" MODIFIED="1592565952053" HGAP_QUANTITY="76.24999814480549 pt" VSHIFT_QUANTITY="-20.999999374151248 pt">
+<node TEXT="Division de l&apos;histoire sous différentes parties et chapitres" ID="ID_518345732" CREATED="1592565784140" MODIFIED="1592565804502"/>
+<node TEXT="Possiblité de résumer l&apos;histoire avec blocs pour chaque partie" ID="ID_1112443665" CREATED="1592565805539" MODIFIED="1592565834813"/>
+<node TEXT="Ajout de références" ID="ID_278349020" CREATED="1592565835380" MODIFIED="1592565862411">
+<node TEXT="Références géographiques" ID="ID_1778853928" CREATED="1592565864679" MODIFIED="1592565877912"/>
+<node TEXT="Références bibliographiques" ID="ID_881094687" CREATED="1592565879117" MODIFIED="1592565889691"/>
+<node TEXT="Actualités" ID="ID_792775077" CREATED="1592565890523" MODIFIED="1592565902236"/>
+<node TEXT="Ordonner dans des dossiers" ID="ID_674481674" CREATED="1592565903053" MODIFIED="1592565921504"/>
+</node>
+</node>
+<node TEXT="Contexte géographique" ID="ID_320657617" CREATED="1592564227733" MODIFIED="1592565954629" HGAP_QUANTITY="98.74999747425325 pt" VSHIFT_QUANTITY="27.74999917298558 pt">
+<node TEXT="Création d&apos;une map" ID="ID_1434279888" CREATED="1592565722857" MODIFIED="1592565733207">
+<node TEXT="En ajoutant des formes géométriques" ID="ID_494183803" CREATED="1592565745229" MODIFIED="1592565760110"/>
+<node TEXT="Pointer vers une référence" ID="ID_1579250492" CREATED="1592565760668" MODIFIED="1592565770713"/>
+</node>
+<node TEXT="Ajouter une map - Google Map" ID="ID_450587881" CREATED="1592565734010" MODIFIED="1592565742273"/>
+</node>
+<node TEXT="Création de personnages" ID="ID_336686053" CREATED="1592563884281" MODIFIED="1592566631985" HGAP_QUANTITY="74.74999818950897 pt" VSHIFT_QUANTITY="84.74999747425325 pt">
+<node TEXT="Ajout d&apos;attributs personnalisés" ID="ID_1679726027" CREATED="1592564284115" MODIFIED="1592564305484"/>
+<node TEXT="Résumer la vie du personnage" ID="ID_1155019860" CREATED="1592564606456" MODIFIED="1592564624300"/>
+<node TEXT="Création des différentes faces du personnage" ID="ID_1834622568" CREATED="1592564625234" MODIFIED="1592564644183">
+<node TEXT="Banque avec images références" ID="ID_192731345" CREATED="1592564709199" MODIFIED="1592564731790"/>
+</node>
+<node TEXT="Ajout d&apos;images de références" ID="ID_348160546" CREATED="1592564646703" MODIFIED="1592564665274"/>
+<node TEXT="Choix d&apos;un nom" ID="ID_211536040" CREATED="1592564741216" MODIFIED="1592564748230">
+<node TEXT="Ajout de plusieurs noms" ID="ID_539915508" CREATED="1592564750888" MODIFIED="1592564761208"/>
+<node TEXT="Ajout d&apos;alias" ID="ID_1593207256" CREATED="1592564765586" MODIFIED="1592564770404"/>
+<node TEXT="Générateur de nom aléatoire" ID="ID_1522621584" CREATED="1592564770990" MODIFIED="1592564784435"/>
+</node>
+<node TEXT="Création de designs communs" ID="ID_452618808" CREATED="1592564665916" MODIFIED="1592564688178"/>
+<node TEXT="Banque de formes de bases" ID="ID_589083997" CREATED="1592564689097" MODIFIED="1592564700670"/>
+<node TEXT="Céation de page de liaision" ID="ID_1516777287" CREATED="1592566634489" MODIFIED="1592566642547">
+<node TEXT="Créer des liens entre les personnages" ID="ID_371171683" CREATED="1592566644385" MODIFIED="1592566658237"/>
+</node>
+</node>
+<node TEXT="Type de projet" ID="ID_1452522322" CREATED="1592564264695" MODIFIED="1592566611421" HGAP_QUANTITY="11.750000067055224 pt" VSHIFT_QUANTITY="109.4999967366458 pt">
+<node TEXT="Bande dessinée" ID="ID_306231494" CREATED="1592564997867" MODIFIED="1592565009029">
+<node TEXT="Décomposition des pages sous différentes caes" ID="ID_1359121160" CREATED="1592565345173" MODIFIED="1592565385902">
+<node TEXT="Création de cases hors marges" ID="ID_379134240" CREATED="1592565480545" MODIFIED="1592565500686"/>
+<node TEXT="Cases contenues dans d&apos;autres cases" ID="ID_1423775853" CREATED="1592565501321" MODIFIED="1592565517442"/>
+<node TEXT="Personnalisation des marges inter-cases" ID="ID_1862425125" CREATED="1592565518148" MODIFIED="1592565534384"/>
+<node TEXT="Possibilité de créer des cases non rectangulaires" ID="ID_820877469" CREATED="1592565534980" MODIFIED="1592565557862"/>
+</node>
+<node TEXT="Ajout des références personnages" ID="ID_1396364835" CREATED="1592565386522" MODIFIED="1592565406816">
+<node TEXT="Avec un croquis, décrivant l&apos;action" ID="ID_1746580910" CREATED="1592565566331" MODIFIED="1592565583778"/>
+<node TEXT="Avec la couleur le caractérisant" ID="ID_1917255265" CREATED="1592565584654" MODIFIED="1592565600748"/>
+</node>
+<node TEXT="Ajout d&apos;arrière plan" ID="ID_1273669378" CREATED="1592565436330" MODIFIED="1592565448810"/>
+<node TEXT="Gestion des bulles" ID="ID_356241520" CREATED="1592565449499" MODIFIED="1592565463017">
+<node TEXT="Gérer la position" ID="ID_1073670507" CREATED="1592565605929" MODIFIED="1592565617332"/>
+<node TEXT="Gérer la taille" ID="ID_1014266314" CREATED="1592565618720" MODIFIED="1592565625902"/>
+<node TEXT="Contenu" ID="ID_788322170" CREATED="1592565626456" MODIFIED="1592565632316"/>
+<node TEXT="Forme de la case" ID="ID_1430690292" CREATED="1592565633636" MODIFIED="1592565640167"/>
+</node>
+<node TEXT="Description" ID="ID_1830898792" CREATED="1592565407447" MODIFIED="1592565430149">
+<node TEXT="Description pour chaque case" ID="ID_215655570" CREATED="1592565645809" MODIFIED="1592565668935"/>
+<node TEXT="Description globale de la page" ID="ID_31061020" CREATED="1592565669450" MODIFIED="1592565681768">
+<node TEXT="Pour situer le contexte par exemple dans l&apos;histoire" ID="ID_1208923942" CREATED="1592565689583" MODIFIED="1592565708476"/>
+</node>
+</node>
+</node>
+<node TEXT="Roman" ID="ID_446511958" CREATED="1592565293086" MODIFIED="1592565297446"/>
+<node TEXT="Jeu de rôle" ID="ID_315964028" CREATED="1592565298011" MODIFIED="1592566611420" HGAP_QUANTITY="14.0 pt" VSHIFT_QUANTITY="83.24999751895673 pt"/>
+</node>
+</node>
+<node TEXT="Gestion de l&apos;interface graphique" POSITION="left" ID="ID_1237417600" CREATED="1592563762068" MODIFIED="1592568237835" HGAP_QUANTITY="90.49999772012241 pt" VSHIFT_QUANTITY="-64.49999807775026 pt">
+<edge COLOR="#0000ff"/>
+<node TEXT="Gestion de la fenêtre" ID="ID_1642147981" CREATED="1592566033072" MODIFIED="1592568234550" HGAP_QUANTITY="11.750000067055222 pt" VSHIFT_QUANTITY="-47.99999856948857 pt"/>
+<node TEXT="Gestion du menu" ID="ID_1589711325" CREATED="1592566043026" MODIFIED="1592568237834" HGAP_QUANTITY="22.999999731779106 pt" VSHIFT_QUANTITY="-26.249999217689062 pt"/>
+<node TEXT="Gestion de la barre d&apos;outil" ID="ID_229357759" CREATED="1592566057345" MODIFIED="1592566563666"/>
+<node TEXT="Gestion du fenêtrage" ID="ID_554930272" CREATED="1592566566183" MODIFIED="1592568226140" HGAP_QUANTITY="71.74999827891594 pt" VSHIFT_QUANTITY="-2.2499999329447764 pt">
+<node TEXT="Un menu latéral gauche" ID="ID_835568671" CREATED="1592566811706" MODIFIED="1592566855329">
+<node TEXT="Liste des références" ID="ID_1693347897" CREATED="1592567784500" MODIFIED="1592567820933"/>
+<node TEXT="Liste des pages" ID="ID_1715406598" CREATED="1592567824878" MODIFIED="1592567834637"/>
+<node TEXT="Liste des élements de projets" ID="ID_352904004" CREATED="1592567875539" MODIFIED="1592568136882"/>
+<node TEXT="Liste des chapitres et parties de l&apos;histoire" ID="ID_1965826063" CREATED="1592567994172" MODIFIED="1592568207478"/>
+</node>
+<node TEXT="Un arbre - voir ses fichiers" ID="ID_130743259" CREATED="1592566863363" MODIFIED="1592567380478"/>
+<node TEXT="Un espace de travail" ID="ID_1824190568" CREATED="1592567394414" MODIFIED="1592567401403">
+<node TEXT="Diviser l&apos;espace de travail en deux écrans" ID="ID_580251168" CREATED="1592567406899" MODIFIED="1592567421430"/>
+</node>
+<node TEXT="Barre de propriétés" ID="ID_860438931" CREATED="1592567430866" MODIFIED="1592567520040"/>
+<node TEXT="menu latéral droit" ID="ID_1812059458" CREATED="1592567572303" MODIFIED="1592567585783">
+<node TEXT="voir la listes de personnages" ID="ID_1114654467" CREATED="1592567747592" MODIFIED="1592567762192"/>
+</node>
+</node>
+<node TEXT="Gestion du bas de pages" ID="ID_1472078330" CREATED="1592566588939" MODIFIED="1592568228323" HGAP_QUANTITY="22.249999754130847 pt" VSHIFT_QUANTITY="29.999999105930357 pt"/>
+</node>
+</node>
+</map>
